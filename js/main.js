fe7179869a5ee7cffe4e62ea4d9ec6bde7af66f0
Ivan Mikerin <vmik7@yandex.ru>
2021-11-29 22:32:30 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -22,6 +22,77 @@ const productGallerySwiper = new Swiper('.product-gallery__swiper', {
         bulletActiveClass: 'product-gallery__paggination-bullet-active',
     },
 });
+const homeSwiper = new Swiper('.home-slider', {
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true,
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+});
+const brand1Swiper = new Swiper('#tires-brand-slider', {
+    slidesPerView: 1,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    pagination: {
+        el: '.brand-slider__pagination',
+        type: 'bullets',
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        620: {
+            slidesPerView: 2,
+        },
+        900: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 4,
+        },
+        1465: {
+            slidesPerView: 5,
+        },
+        1730: {
+            slidesPerView: 6,
+        },
+    },
+});
+const brand2Swiper = new Swiper('#disks-brand-slider', {
+    slidesPerView: 1,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    pagination: {
+        el: '.brand-slider__pagination',
+        type: 'bullets',
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        620: {
+            slidesPerView: 2,
+        },
+        900: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 4,
+        },
+        1465: {
+            slidesPerView: 5,
+        },
+        1730: {
+            slidesPerView: 6,
+        },
+    },
+});
 
 $.fn.responsiveTabs = function () {
     this.addClass('responsive-tabs');
@@ -40,4 +111,5 @@ $.fn.responsiveTabs = function () {
 $('.product-info__tabs').responsiveTabs();
 $('.tirefit-price__tabs').responsiveTabs();
 $('.wheel-size-list').responsiveTabs();
+$('.tires-season__tabs').responsiveTabs();
 /* eslint-enable no-undef, no-unused-vars */
